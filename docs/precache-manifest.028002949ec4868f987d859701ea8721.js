43f48f4d91b366b74df350ca4b1d9f1a0050ff95
msrx9 <msrx9g@gmail.com>
2018-09-03 22:42:00 +0900

    first commit

---
+++ docs/precache-manifest.028002949ec4868f987d859701ea8721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-sample/robots.txt"
+  },
+  {
+    "revision": "2058ea4abe811947451a",
+    "url": "/pwa-sample/js/chunk-vendors.5b240b47.js"
+  },
+  {
+    "revision": "7daa6c7f305c9ca4b080",
+    "url": "/pwa-sample/js/app.747705b6.js"
+  },
+  {
+    "revision": "083b480c704835c0eac2",
+    "url": "/pwa-sample/js/about.5d688836.js"
+  },
+  {
+    "revision": "f7e622cc2293866ce838390f6b5ca7c0",
+    "url": "/pwa-sample/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-sample/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "7daa6c7f305c9ca4b080",
+    "url": "/pwa-sample/css/app.a8f31159.css"
+  }
+];
